2a600802ef3d3f5c8c2d49b64b5172e87b78e43f
Yvan Deniro <azanfacknkenhoungyvandeniro@gmail.com>
2023-11-13 16:29:17 +0100

    post template

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         return view('posts.index',[
             'posts' => Post::latest()->paginate(10),
+        ]);
+
+    }
+
+    public function show(Post $post)
+    {
+
+        return view('posts.show', [
+            'post' => $post,
         ]);
 
     }
