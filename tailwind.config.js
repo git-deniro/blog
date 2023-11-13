7d4cadbf5e4d9b957f04a5a12f958250d7904f4f
Yvan Deniro <azanfacknkenhoungyvandeniro@gmail.com>
2023-11-13 08:08:08 +0100

    Vite configuration

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'base', // only generate global styles
+      strategy: 'class' //only generate classes
+    }),
+  ],
+}
+
